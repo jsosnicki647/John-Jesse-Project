2fc0993d96556c9036fc0a02004801f3c2298d95
Jesse Sosnicki <jesse.sosnicki@gmail.com>
2020-04-21 19:11:17 -0700

    initialize server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.htm')
+})
+
+app.listen(PORT, () => {
+    console.log('App listening on PORT ' + PORT)
+    console.log(__dirname)
+})
